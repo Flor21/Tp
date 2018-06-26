be51134568606bfe9df55de0bf99aee1794a81ec
Flor21 <fas-2196@hotmail.com>
2018-06-25 21:56:51 -0300

    No es el final del docker

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo docker run --rm -u root -p 8080:8080 -v /var/jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home/flor/TpFinal jenkinsci/blueocean
+sudo docker run --rm -u root -p 8080:8080 -v /home/flor/jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home/flor/TpFinal jenkinsci/blueocean
